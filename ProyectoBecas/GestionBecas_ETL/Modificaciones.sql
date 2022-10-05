f1f2a2704a61b0dd620f6534e261e952727ed6d8
Bismark Garcia <80867661+Steve99Garcia@users.noreply.github.com>
2022-10-04 23:30:47 -0600

    Add files via upload
    proyecto de base de datos primer agregado

---
+++ ProyectoBecas/GestionBecas_ETL/Modificaciones.sql
@@ -0,0 +1,9 @@
+Use GestionBecas
+
+Update Programa set Duracion='16meses' where IDPrograma=1
+
+Select * from Programa
+
+Use GestionBecasDW
+
+Select DISTINCT * from DimPrograma where IDPrograma=1
